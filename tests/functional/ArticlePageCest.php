734e87347f82dbceeea71f2d2fcbc1c93c3ff6b9
magzhan <magzhanomirtay@gmail.com>
2021-05-27 17:39:04 +0600

    gitignore configured

--- tests/functional/ArticlePageCest.php
+++ tests/functional/ArticlePageCest.php
@@ -2,12 +2,17 @@
 
 class ArticlePageCest
 {
-    public function _before(FunctionalTester $I)
-    {
-    }
-
     // tests
-    public function tryToTest(FunctionalTester $I)
+    public function checkArticlePageThroughTheList(FunctionalTester $I)
     {
+        $I->amOnPage('');
+        $I->seeElement('#navbar-links > li:nth-child(2) > a');
+        $I->click('#navbar-links > li:nth-child(2) > a');
+        $I->seeElement('#post_558434 > article > h2 > a');
+
+        codecept_debug($I->grabTextFrom('#navbar-links > li:nth-child(2) > a'));
+        
+        $I->click('#post_558434 > article > h2 > a');
+        $I->seeElement('body > div.layout > div.layout__row.layout__row_body > div > div.column-wrapper.js-sticky-wrapper > div.content_left.js-content_left > div');
     }
 }
